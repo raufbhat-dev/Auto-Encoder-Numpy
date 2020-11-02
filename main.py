a7440618511c66f5f2e83c704636bf842765b259
raufbhat <rauf_bhat@abfldirect.com>
2020-11-02 15:58:59 +0000

    New Outputs added

--- main.py
+++ main.py
@@ -1,74 +1,81 @@
+
 import numpy as np
 import sklearn.datasets
-import matplotlib.pyplot as plt 
 import matplotlib
+import matplotlib.pyplot as plt 
 import importlib
 
-from neuralnetnumpy import neuralnet 
-
-NeuralNet = importlib.reload(neuralnet).NeuralNet
+from autoencodernumpy import autoencoder 
+AutoEncoder = importlib.reload(autoencoder).AutoEncoder
 
 inputs, Y = sklearn.datasets.load_digits( n_class=10, return_X_y=True)
-outputs = inputs
 
-outputs = np.zeros((Y.size, Y.max()+1))
-outputs[np.arange(Y.size),Y] = 1
-
-inputs = inputs/255
+inputs = sklearn.preprocessing.scale(inputs)
 
 np.random.seed(0)
 
-network_arch = [{'layer_type':'input', 'size':inputs.shape[-1]},
-                {'layer_type':'hidden', 'size':49, 'activation':'sigmoid'},
-                {'layer_type':'output', 'size':inputs.shape[-1], 'activation':'leakyrelu'}]
+network_arch = [{'layer_type':'input', 'size':inputs.shape[-1],'network_part':'encoder'},
+                {'layer_type':'hidden', 'size':49, 'activation':'sigmoid','network_part':'encoder'},
+                {'layer_type':'output', 'size':inputs.shape[-1], 'activation':'leakyrelu','network_part':'decoder'}]
 
 loss_func = 'meansquared'
 optimiser_method = 'momentum'
 learning_rate = 0.001
-epoch = 2000
+epoch = 50000
 partition = 1
 mode = 'train'
 loss_display_freq = 1
 
-neural_net = NeuralNet(loss_func, optimiser_method, learning_rate, epoch, partition, mode, network_arch,beta =0.9)
+auto_encoder = AutoEncoder(loss_func, optimiser_method, learning_rate, epoch, partition, mode, network_arch,beta =0.9)
 
-neural_net.createNetwork()
-neural_net.train(inputs,inputs)
+auto_encoder.createNetwork()
 
+auto_encoder.train(inputs,inputs)
+
+encoder_layers = auto_encoder.encoder_layer_list
+decoder_layers = auto_encoder.decoder_layer_list
 
 #*********************** Ploting Outputs ***************
+auto_encoder.mode = 'test'
 
-neural_net.mode = 'test'
-img_index=10
-test_out_sample = neural_net.forwardPass(inputs[img_index], 1)
+img_index = 1
+test_out_sample = auto_encoder.forwardPass(inputs[img_index], 1)
 
 #actual
+print('Actual image for index:{}'.format(img_index))
 plt.gray() 
 plt.imshow(inputs[img_index].reshape(8,8),interpolation='bilinear', cmap=plt.cm.Greys_r) 
 plt.show() 
 
 #regenrated
+print('Reconstructed image for index:{}'.format(img_index))
 plt.imshow(test_out_sample.reshape(8,8),interpolation='bilinear', cmap=plt.cm.Greys_r) 
 plt.show() 
 
+
 #*********************** Ploting weights ***************
 
-#layer1
-fig=plt.figure(figsize=(10, 10))
-columns = 7
-rows = 7
-for i in range(columns*rows):
-    layer2 = neural_net.layers_list[0].w.T[i].reshape(8,8)
-    fig.add_subplot(rows, columns, i+1)
-    plt.imshow(layer2,interpolation='bilinear', cmap=plt.cm.Greys_r)
-plt.show()
+print('Weights')
+#encoder
+fig = plt.figure(figsize=(10, 10))
+for index, _layer in enumerate(encoder_layers):
+    rows = columns = 7
+    print('Encoder layer: {}'.format(index))
+    image_shape = 8
+    for i in range(columns*rows):
+        layer2 = _layer.w.T[i].reshape(image_shape,image_shape)
+        fig.add_subplot(rows, columns, i+1)
+        plt.imshow(layer2,interpolation='bilinear', cmap=plt.cm.Greys_r)
+    plt.show()
 
-
-#layer2
-columns = 8
-rows = 8
-for i in range(columns*rows):
-    layer2 = neural_net.layers_list[1].w.T[i].reshape(7,7)
-    fig.add_subplot(rows, columns, i+1)
-    plt.imshow(layer2,interpolation='bilinear', cmap=plt.cm.Greys_r)
-plt.show()
+#decoder
+fig = plt.figure(figsize=(10, 10))
+for index, _layer in enumerate(decoder_layers):
+    rows = columns = 8
+    print('Decoder layer: {}'.format(index))
+    image_shape = 7
+    for i in range(columns*rows):
+        layer2 = _layer.w.T[i].reshape(image_shape,image_shape)
+        fig.add_subplot(rows, columns, i+1)
+        plt.imshow(layer2,interpolation='bilinear', cmap=plt.cm.Greys_r)
+    plt.show()
